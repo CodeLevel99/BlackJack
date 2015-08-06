306d0ebf1ab3ab0cbc42fa00c34ac060f5897ae0
Dormeus <fedor_lisovskiy@zoho.com>
2015-08-05 22:04:29 -0400

    Create deck.py

---
+++ deck.py
@@ -0,0 +1,17 @@
+deck = {}
+#Spades = 100
+#Clubs = 200
+#Diamonds = 300
+#Hearts = 400
+for x in range (1, 53):
+    if x<=13:
+       deck[x] = 100 +  x
+    if x>13 and x<=26:
+       deck[x] = 200 + x - 13
+    if x>26 and x<=39:
+       deck[x] = 300 +  x - 26
+    if x>39:
+       deck[x] = 400 + x - 39
+
+for x in range (1, 53):
+    print (deck[x])
