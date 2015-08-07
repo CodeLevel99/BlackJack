a0ca65c61770cba908d772ee10fd5d74ba138e41
Derek Shao <shao.derek.now@gmail.com>
2015-08-07 00:29:18 -0700

    commitment

---
+++ hand.py
@@ -0,0 +1,67 @@
+
+from deck import *
+import random
+
+class Hand:
+    def __init__(self, size, owner):
+        self.size = 0
+        self.owner = owner
+        self.cards = {}
+    def get(self, card):
+        self.size = self.size + 1
+        self.cards[self.size] = card
+    def show_hand(self):
+        for x in range (1, self.size+1):
+            print(self.cards[x])
+
+def deal(target):
+    draw = deck[random.randrange (1, 53, 1)]
+    target.get(draw)
+    return draw
+
+def name(card):
+   if int(card/100) == 1:
+       suit = "Sp"
+   elif int(card/100) == 2:
+       suit = "Cb"
+   elif int(card/100) == 3:
+       suit = "Dm"
+   else:
+       suit = "Ht"
+
+   if card%100 == 1:
+       value = "Ace"
+   elif card%100 == 11:
+       value = "Jack"
+   elif card%100 == 12:
+       value = "Queen"
+   elif card%100 == 13:
+       value = "King"
+   else:
+       value = str(card%100)
+   return  suit + value
+
+def check(card, target):
+    if card%100 == 11 or card%100 == 12 or card%100 == 13:
+        cardValue = 10
+    elif card%100 == 1:
+        for x in range (1, target.size+1):
+            if target.cards[x]%100 == 11 or target.cards[x]%100 == 12 or target.cards[x]%100 == 13:
+                cardValue = 11
+                break
+            else:
+                cardValue = 1
+    else:
+        cardValue = card%100
+        
+    return cardValue
+
+def handvalue(target):
+    current = 0
+    for x in range(1, target.size+1):
+        current = current + check(target.cards[x], target)
+    return current
+
+
+#hello
+
