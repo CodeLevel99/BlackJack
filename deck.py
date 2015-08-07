760f632410ef9e397a2eeaefac52ebec00dcd445
Derek Shao <shao.derek.now@gmail.com>
2015-08-07 13:08:34 -0700

    Shrinking deck patch

--- deck.py
+++ deck.py
@@ -1,14 +1,15 @@
 deck = {}
-#Spades = 100
-#Clubs = 200
-#Diamonds = 300
-#Hearts = 400
-for x in range (1, 53):
-    if x<=13:
-       deck[x] = 100 +  x
-    if x>13 and x<=26:
-       deck[x] = 200 + x - 13
-    if x>26 and x<=39:
-       deck[x] = 300 +  x - 26
-    if x>39:
-       deck[x] = 400 + x - 39
+def fresh_deck():
+    #Spades = 100
+    #Clubs = 200
+    #Diamonds = 300
+    #Hearts = 400
+    for x in range (1, 53):
+        if x<=13:
+            deck[x] = 100 +  x
+        if x>13 and x<=26:
+            deck[x] = 200 + x - 13
+        if x>26 and x<=39:
+            deck[x] = 300 +  x - 26
+        if x>39:
+            deck[x] = 400 + x - 39
